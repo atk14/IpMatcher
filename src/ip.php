b6d132423f881c8d26d5953d8a518b5eea79068e
Michal Vojan <michal.vojan@gmail.com>
2015-11-23 15:34:40 +0100

    testing IPv6

--- src/ip.php
+++ src/ip.php
@@ -2,6 +2,8 @@
 /**
  * Simple class for checking IP address suits IP mask.
  *
+ * It can check both IPv4 and IPv6 addresses.
+ *
  * @package Atk14\Ip
  * @filesource
  */
@@ -9,14 +11,22 @@
 /**
  * Simple class for checking IP address suits IP mask.
  *
- * Compare two IPs
+ * Compare two IPs. If the parameter $cidr is passed in the CIDR form, it takes it as a subnet or range of ip addresses
+ * and tests if the $ip belongs to given subnet
+ *
+ * There are two basic methods for matching IPv4 and IPv6 addresses (Match4() and Match6()).
+ * Also method Match() can be used which should recognize if the IP is v4 or v6 type.
+ *
+ * Basic use
  * ```
  * IP::Match("127.0.0.1", "127.0.0.1");
  * ```
  *
  * Check an IP belongs to a 127.0.0.1-127.0.0.255 subnet
+ * Subnet mask can be given as integer or a dot mask.
  * ```
  * IP::Match("127.0.0.1", "127.0.0.1/24");
+ * IP::Match("127.0.0.1", "127.0.0.1/255.255.255.0");
  * ```
  * Check an IP matches at least one of addresses/subnets specified in an array
  * ```
@@ -28,32 +38,41 @@
  */
 class IP {
 
+	static function Match($ip, $cidr) {
+		if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+			return self::Match4($ip, $cidr);
+		} elseif (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+			return self::Match6($ip, $cidr);
+		}
+	}
+
 	/**
 	 * Checks if IP address matches another IP or subnet.
 	 *
-	 * @param string $ip
-	 * @param string|array $adr_mask
-	 * @return boolean
+	 * @param string $ip tested ip address
+	 * @param string|array $cidr
+	 * @return boolean true - $ip is the same as $cidr or belongs to the subnet
 	 */
-	static function Match($ip, $adr_mask) {
-		if (is_array($adr_mask)) {
-			foreach($adr_mask as $mask) {
+	static function Match4($ip, $cidr) {
+		if (is_array($cidr)) {
+			foreach($cidr as $mask) {
 				if (self::Match($ip, $mask)) {
 					return true;
 				}
 			}
 			return false;
 		}
-		list($subnet,$mask) = preg_split('/\//', $adr_mask."/");
+		list($subnet,$mask) = preg_split('/\//', $cidr."/");
 
 		# default mask
 		$bits = 32;
 		if (preg_match("/^(\d{1,2})$/", $mask, $matches)) {
 			$bits = $matches[1];
 		}
+		// mask can be given in dot form (ie 255.255.0.0)
 		// TODO: lepsi regexp?
 		if (preg_match("/^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/", $mask, $matches)) {
-			$bits = IP::_Mask2cidr($mask);
+			$bits = self::_Mask2cidr($mask);
 			# pri nejake divne masce, ze ktere nevzejde cele cislo vracime false
 			if ((float)(int)$bits != $bits) {
 				return false;
@@ -67,6 +86,52 @@ class IP {
 	}
 
 	/**
+	 * Checks if na IPv6 address suits a subnet.
+	 *
+	 * If $cidr is an IP address, it just compares the $ip and $cidr
+	 *
+	 * @param string $ip tested ip address
+	 * @param string|array $cidr
+	 * @return boolean true - $ip is the same as $cidr or belongs to the subnet
+	 */
+	static function Match6($ip, $cidr) {
+		if (is_array($cidr)) {
+			foreach($cidr as $mask) {
+				if (self::Match6($ip, $mask)) {
+					return true;
+				}
+			}
+			return false;
+		}
+		$ip = inet_pton($ip);
+		if ($ip===false) { return false; }
+
+		list($net,$maskbits) = explode('/',"$cidr");
+		$net = inet_pton($net);
+		if ($net===false) { return false; }
+
+		$binaryip = self::InetToBits($ip);
+		$binarynet = self::InetToBits($net);
+
+		$ip_net_bits = substr($binaryip,0,$maskbits);
+		$net_bits = substr($binarynet,0,$maskbits);
+
+		return ($ip_net_bits==$net_bits);
+	}
+
+	// converts inet_pton output to string with bits
+	private function InetToBits($inet)
+	{
+		$unpacked = unpack('A16', $inet);
+		$unpacked = str_split($unpacked[1]);
+		$binaryip = '';
+		foreach ($unpacked as $char) {
+			$binaryip .= str_pad(decbin(ord($char)), 8, '0', STR_PAD_LEFT);
+		}
+		return $binaryip;
+	}
+
+	/**
 	 * Prevede teckovou masku na cidr masku
 	 * Pokud je maska nevalidni (napr. 255.240.255.0), vracena hodnota bude mit nenulovou desetinnou cast
 	 *
@@ -77,5 +142,4 @@ class IP {
 		$base = ip2long('255.255.255.255');
 		return 32-log(($long ^ $base)+1,2);
 	}
-			  
 }
--- src/test/tc_ip.php
+++ src/test/tc_ip.php
@@ -1,19 +1,57 @@
 <?php
 class tc_ip extends tc_base {
-	function test() {
-		$this->assertTrue(IP::Match("195.100.100.1", "195.100.100.1"));
 
-		$this->assertTrue(IP::Match("195.100.100.1", "195.100.100.1/32"));
-		$this->assertFalse(IP::Match("195.100.100.2", "195.100.100.1/32"));
-		
-		$this->assertTrue(IP::Match("195.100.100.1", "195.100.100.0/24"));
-		$this->assertTrue(IP::Match("195.100.100.1", "195.100.100.255/24"));
-		$this->assertFalse(IP::Match("195.100.101.1", "195.100.100.255/24"));
+	function test_ipv6() {
+		$tested_subnets = array(
+			array(
+				"ip" => "21DA:00D3:0000:2F3B:02AC:00FF:FE28:9C5A",
+				"cidr" => "21DA:00D3:0000:2F3B::/64",
+				"expected_result" => true,
+			),
+			array(
+				"ip" => "21DA:00D3:0000:2F3B:04AC:01FF:FE28:9C5A",
+				"cidr" => "21DA:00D3:0000:2F3B::/64",
+				"expected_result" => true,
+			),
+			array(
+				"ip" => "21DB:00D3:0000:2F3B:02AC:00FF:FE28:9C5A",
+				"cidr" => "21DA:00D3:0000:2F3B::/64",
+				"expected_result" => false,
+			),
+			array(
+				"ip" => "::1",
+				"cidr" => "0::0/64",
+				"expected_result" => true,
+			),
+			array(
+				"ip" => "::1",
+				"cidr" => "1::0/64",
+				"expected_result" => false,
+			),
+			array(
+				"ip" => "::1",
+				"cidr" => "a0::2/4",
+				"expected_result" => true,
+			),
+			array(
+				"ip" => "::1",
+				"cidr" => "a0::2/64",
+				"expected_result" => false,
+			),
+			array(
+				"ip" => "::1",
+				"cidr" => "0::0/64",
+				"expected_result" => true,
+			),
+		);
 
-		$this->assertTrue(IP::Match("195.100.101.1", "195.100.100.255/16"));
-		$this->assertFalse(IP::Match("195.100.100.128", "195.100.100.127/25"));
-		$this->assertTrue(IP::Match("195.100.100.127", "195.100.100.127/25"));
+		foreach($tested_subnets as $idx => $subnet) {
+			$this->assertEquals($subnet["expected_result"], IP::Match6($subnet["ip"], $subnet["cidr"]), sprintf("Failed with idx: %d, %s", $idx, print_r($subnet,true)));
+			$this->assertEquals($subnet["expected_result"], IP::Match($subnet["ip"], $subnet["cidr"]), sprintf("Failed with idx: %d, %s", $idx, print_r($subnet,true)));
+		}
+	}
 
+	function test_ipv4() {
 		$mask_ar = array(
 			"195.100.100.0",
 			"195.100.100.1",
@@ -22,14 +60,97 @@ class tc_ip extends tc_base {
 			"195.100.100.255/16",
 			"195.200.100.127/25"
 		);
-		$this->assertTrue(IP::Match("195.100.100.1", $mask_ar));
-		$this->assertTrue(IP::Match("195.100.101.1", $mask_ar));
-		$this->assertFalse(IP::Match("195.101.101.1", $mask_ar));
-		$this->assertFalse(IP::Match("195.101.100.1", $mask_ar));
-		$this->assertTrue(IP::Match("195.200.100.127", $mask_ar));
-		$this->assertFalse(IP::Match("195.200.100.128", $mask_ar));
+		$tested_subnets = array(
+			array(
+				"ip" => "195.100.100.1",
+				"cidr" => "195.100.100.1",
+				"expected_result" => true,
+			),
+			array(
+				"ip" => "195.100.100.1",
+				"cidr" => "195.100.100.1/32",
+				"expected_result" => true,
+			),
+			array(
+				"ip" => "195.100.100.2",
+				"cidr" => "195.100.100.1/32",
+				"expected_result" => false,
+			),
+			array(
+				"ip" => "195.100.100.1",
+				"cidr" => "195.100.100.0/24",
+				"expected_result" => true,
+			),
+			array(
+				"ip" => "195.100.100.1",
+				"cidr" => "195.100.100.255/24",
+				"expected_result" => true,
+			),
+			array(
+				"ip" => "195.100.101.1",
+				"cidr" => "195.100.100.255/24",
+				"expected_result" => false,
+			),
+			array(
+				"ip" => "195.100.101.1",
+				"cidr" => "195.100.100.255/16",
+				"expected_result" => true,
+			),
+			array(
+				"ip" => "195.100.101.1",
+				"cidr" => "195.100.100.255/255.255.0.0",
+				"expected_result" => true,
+			),
+			array(
+				"ip" => "195.100.100.128",
+				"cidr" => "195.100.100.127/25",
+				"expected_result" => false,
+			),
+			array(
+				"ip" => "195.100.100.127",
+				"cidr" => "195.100.100.127/25",
+				"expected_result" => true,
+			),
+			array(
+				"ip" => "195.100.100.127",
+				"cidr" => "195.100.100.127/255.255.255.128",
+				"expected_result" => true,
+			),
+			array(
+				"ip" => "195.100.100.1",
+				"cidr" => $mask_ar,
+				"expected_result" => true,
+			),
+			array(
+				"ip" => "195.100.101.1",
+				"cidr" => $mask_ar,
+				"expected_result" => true,
+			),
+			array(
+				"ip" => "195.101.101.1",
+				"cidr" => $mask_ar,
+				"expected_result" => false,
+			),
+			array(
+				"ip" => "195.101.100.1",
+				"cidr" => $mask_ar,
+				"expected_result" => false,
+			),
+			array(
+				"ip" => "195.200.100.127",
+				"cidr" => $mask_ar,
+				"expected_result" => true,
+			),
+			array(
+				"ip" => "195.200.100.128",
+				"cidr" => $mask_ar,
+				"expected_result" => false,
+			),
+		);
 
-
-
+		foreach($tested_subnets as $idx => $subnet) {
+			$this->assertEquals($subnet["expected_result"], IP::Match4($subnet["ip"], $subnet["cidr"]), sprintf("Failed with idx: %d, %s", $idx, print_r($subnet,true)));
+			$this->assertEquals($subnet["expected_result"], IP::Match($subnet["ip"], $subnet["cidr"]), sprintf("Failed with idx: %d, %s", $idx, print_r($subnet,true)));
+		}
 	}
 }
